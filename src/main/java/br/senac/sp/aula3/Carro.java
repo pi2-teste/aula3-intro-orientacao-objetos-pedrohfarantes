0a393b2cdb19898e22564a964690a5e911208213
pedrohfarantes <phfarantes96@gmail.com>
2019-09-05 11:36:51 -0300

    atividade

--- src/main/java/br/senac/sp/aula3/Carro.java
+++ src/main/java/br/senac/sp/aula3/Carro.java
@@ -5,10 +5,115 @@
  */
 package br.senac.sp.aula3;
 
+//import static br.senac.sp.aula3.Main.assertEqual;
+//import java.util.Arrays;
+//import javax.swing.JOptionPane;
 /**
  *
  * @author fernando.fernandes
  */
 public class Carro {
-    
+
+    private static String fabricante;
+    private String[] rodas;
+    private String cor;
+    private String modelo;
+    private int velocidadeAtual;
+    private int gasolina;
+    private int max_gasolina;
+
+    //Defina um construtor vazio para a classe
+    public Carro() {
+        this.max_gasolina = 40;
+        this.gasolina = 20;
+    }
+
+    //Crie os métodos acessores getters
+    public static String getFabricante() {
+        return fabricante;
+    }
+
+    public String[] getRodas() {
+        return rodas;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getVelocidadeAtual() {
+        return velocidadeAtual;
+    }
+
+    public int getGasolina() {
+        return gasolina;
+    }
+
+    public int getMax_gasolina() {
+        return max_gasolina;
+    }
+
+    //Crie os métodos acessores setters
+    public static void setFabricante(String fabricante) {
+        Carro.fabricante = fabricante;
+    }
+
+    public void setRodas(String[] rodas) {
+        this.rodas = rodas;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setVelocidadeAtual(int velocidadeAtual) {
+        this.velocidadeAtual = velocidadeAtual;
+    }
+
+    public void setGasolina(int gasolina) {
+        this.gasolina = gasolina;
+    }
+
+    public void setMax_gasolina(int max_gasolina) {
+        this.max_gasolina = max_gasolina;
+    }
+
+    //Crie uma método "descreve"
+    public String descreve() {
+
+        String descricao = null;
+
+        descricao = "Um " + fabricante + modelo + " na cor " + cor;
+
+        if (modelo == null) {
+            descricao = "Um " + modelo + " na cor " + cor;
+        } else if (cor == null) {
+            descricao = "Um " + fabricante + modelo;
+        } else if (modelo == null) {
+            descricao = "Um carro";
+        }
+
+        return descricao;
+
+    }
+
+    public void abastece(int litros) {
+
+        if ((gasolina + litros) >= max_gasolina) {
+            gasolina = 40;
+        } else {
+
+            gasolina = (gasolina + litros);
+        }
+
+    }
+
 }
--- src/main/java/br/senac/sp/aula3/Main.java
+++ src/main/java/br/senac/sp/aula3/Main.java
@@ -12,22 +12,22 @@ import javax.swing.JOptionPane;
  * @author lucas.mendes
  */
 public class Main {
-    
+
     public static void assertEqual(int a, int b) {
-        if (!(a == b)){
-            throw new Error("Esperava que "+a+" fosse igual a "+b);
-        }
-    }
-    public static void assertEqual(String a, String b) {
-        if (!a.equals(b)) {
-            throw new Error("EU esperava que "+a+" fosse igual a "+b);
+        if (!(a == b)) {
+            throw new Error("Esperava que " + a + " fosse igual a " + b);
         }
     }
 
-/*
+    public static void assertEqual(String a, String b) {
+        if (!a.equals(b)) {
+            throw new Error("EU esperava que " + a + " fosse igual a " + b);
+        }
+    }
+
     public static void testar_atributos() {
-        Carro carro1 =  new Carro();
-        Carro carro2 =  new Carro();
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
         carro1.setModelo("Fiat Uno");
         assertEqual(carro1.getModelo(), "Fiat Uno");
         carro2.setModelo("Ford Escort");
@@ -35,24 +35,23 @@ public class Main {
         assertEqual(carro2.getModelo(), "Ford Escort");
         carro2.setCor("prata");
         assertEqual(carro2.getCor(), "prata");
-        System.out.println("Parabéns, o teste de atributos está correto!");  
+        System.out.println("Parabéns, o teste de atributos está correto!");
     }
-*/
-/*
+
     public static void testar_descricao() {
-        Carro carro1 =  new Carro();
-        Carro carro2 =  new Carro();
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
         carro1.setModelo("Fiat Uno");
         carro2.setModelo("Ford Escort");
         carro2.setCor("prata");
         assertEqual(carro2.descreve(), "Um Ford Escort na cor prata");
-        assertEqual(carro1.descreve(),"Um Fiat Uno");
+        assertEqual(carro1.descreve(), "Um Fiat Uno");
         Carro carro3 = new Carro();
         assertEqual(carro3.descreve(), "Um carro");
         System.out.println("Parabéns, o teste da descrição está correto!");
     }
-*/
-/*    
+
+        
     public static void testar_gasolina1() {
         Carro carro1 = new Carro();
         assertEqual(carro1.getGasolina(), 20);
@@ -60,8 +59,8 @@ public class Main {
         assertEqual(carro1.getGasolina(), 30);
         System.out.println("Parabéns, o teste 1 da gasolina está correto!");
     }
-*/
-/*  
+     
+   
     public static void testar_gasolina2() {
         Carro carro1 = new Carro();
         assertEqual(carro1.getGasolina(), 20);
@@ -71,8 +70,8 @@ public class Main {
         assertEqual(carro1.getGasolina(), 40);
         System.out.println("Parabéns, o teste 2 da gasolina está correto!");
     }
-*/
-/*    
+     
+ /*    
     public static void testar_max_gasolina() {
         Carro carro1 = new Carro(30);
         Carro carro2 = new Carro(50);
@@ -88,17 +87,28 @@ public class Main {
         assertEqual(carro2.getGasolina(), 45);
         System.out.println("Parabéns, o teste max gasolina está correto!");
     }
-*/
-    
-     /**
+     */
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //testar_atributos();
-        //testar_descricao();
-        //testar_gasolina1();
-        //testar_gasolina2();
+
+        //Instancio o objeto da classe Carro
+        Carro meuCarro = new Carro();
+        meuCarro.setModelo("SeuModeloDeCarroFavorito");
+
+        //Instancio outro objeto da classe Carro
+        Carro carroRodizio = new Carro();
+        carroRodizio.setModelo("civic");
+        carroRodizio.setCor("prata");
+        
+        JOptionPane.showMessageDialog(null, carroRodizio.getModelo() + " - " + carroRodizio.getCor());
+        
+        testar_atributos();
+        testar_descricao();
+        testar_gasolina1();
+        testar_gasolina2();
         //testar_max_gasolina();
     }
-    
+
 }
